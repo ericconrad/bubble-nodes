5b84d557476c1399be1d099daeb2372afcb93ab9
Eric Conrad <eric.jeffrey.conrad@gmail.com>
2015-02-09 15:27:02 -0500

    bubbles with centered text, svg lines connect, responsive FTW

--- src/js/main.js
+++ src/js/main.js
@@ -3,7 +3,5 @@ var log = require("./lib/log"),
 
 
 $(function () {
-
-  log("The document is ready and we have some js running, way to go. Big accomplishment. We're all pulling for you.");
-
+  
 });
